0316455e7b928ac2426b7c824a5ca027fa64ad15
Pascal7601 <levispascal63@gmail.com>
2025-10-06 12:41:44 +0300

    created a simple todo list to enable users to add, delete the tasks

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask() {
+        // obtain the value from input and remove extra spaces
+        let taskText = taskInput.value.trim();
+        if(taskText === "") {
+            alert("enter a task")
+        }
+        // create an li element to be displayed under the button
+        let li = document.createElement("li");
+        li.append(taskText);
+        // crteate a btn to remove tasks
+        const btn = document.createElement("button");
+        btn.textContent = "Remove";
+        btn.className = "remove-btn"
+        li.append(btn);
+        // remove a task when the remove btn is clicked
+        btn.addEventListener("click", () => {
+            li.remove(taskText);
+        })
+
+        taskList.appendChild(li);
+        taskInput.value = "";
+        
+    }
+    taskInput.addEventListener("keypress", (e) => {
+        // check when user clicks enter key to add tasts
+        if(e.key == "Enter") {
+            addTask();
+        }
+    })
+    addButton.addEventListener("click", addTask);
+})
